4a7da4d1e1b1166d79072ba29c5a6ef6c60d568a
David Xie <david.scriptfan@gmail.com>
2016-11-29 22:24:06 +0000

    fix errors

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,7 +1,10 @@
+import 'rxjs/add/operator/switchMap';
+
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
+import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
@@ -12,6 +15,8 @@ import { HeroService } from './hero.service';
 })
 
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
@@ -27,6 +32,4 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
-
-  @Input() hero: Hero;
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router }  from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: [ 'heroes.component.css' ],
-  providers: [HeroService]
+  styleUrls: [ 'heroes.component.css' ]
 })
 
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
 
